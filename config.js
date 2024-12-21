b382e64c091f7f73a218740980c6ea28010a0991
suranga-chamith <schamith036@gmail.com>
2024-12-21 18:21:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'add your sesson id',
+    SESSION_ID: process.env.SESSION_ID || '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=q2Z2iBzK#uAuZlkvi8uiecHg8obrPOAIZJ5gT7BSVdxHxEAs4yqo',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 };
