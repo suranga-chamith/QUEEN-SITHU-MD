12b0c2ffa2bed6ec6b609d6f3463b311854d36d8
THARU44 <ttharumd@gmail.com>
2024-12-21 16:46:25 +0530

    Create ai.js

---
+++ plugins/ai.js
@@ -0,0 +1,148 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const { fetchJson } = require('../lib/functions');
+const fs=require('fs');
+const path=require('path');
+const{readEnv}=require('../lib/database');
+var desct = "It Search On Chatgpt Ai For What You Provided."
+var needus = "*Please Give Me Words To Search On AI !*" 
+var cantf  = "*Server Is Busy. Try Again Later.!*"
+
+
+cmd({
+    pattern: "ai",
+    react: '🤖',
+    desc: desct,
+    category: "ai",
+    use: '.chatgpt <query>',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply(needus)
+//let res = (await fetchJson('https://hercai.onrender.com/v3/hercai?question=' + q)).response
+let res = await fetchJson('https://hercai.onrender.com/v3/hercai?question='+q)
+
+return await reply(res.reply)
+} catch (e) {
+reply(cantf)
+console.log(e)
+}
+})
+
+//==============================CHATGPT============================
+
+cmd({
+    pattern: "chatgpt",
+    alias: ["gpt","openai","chat"],
+    react: '🤖',
+    desc: desct,
+    category: "ai",
+    use: '.chatgpt <query>',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply(needus)
+//let res = (await fetchJson('https://hercai.onrender.com/v3/hercai?question=' + q)).response
+let res = await fetchJson('https://hercai.onrender.com/v3/hercai?question='+q)
+
+return await reply(res.reply)
+} catch (e) {
+reply(cantf)
+console.log(e)
+}
+})
+//==============================CHATGPT2============================
+
+cmd({
+    pattern: "chatgpt2",
+    alias: ["ai2","gpt2","openai2","chat2"],
+    react: '🤖',
+    desc: desct,
+    category: "ai",
+    use: '.chatgpt2 <query>',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply(needus)
+//let res = (await fetchJson('https://hercai.onrender.com/v3/hercai?question=' + q)).response
+let res = await fetchJson('https://hercai.onrender.com/v3-32k/hercai?question='+q)
+
+return await reply(res.reply)
+} catch (e) {
+reply(cantf)
+console.log(e)
+}
+})
+//==============================TURBO============================
+
+cmd({
+    pattern: "turbo",
+    alias: ["ai3","gpt3","openai3","chat3"],
+    react: '👾',
+    desc: desct,
+    category: "ai",
+    use: '.turbo <query>',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply(needus)
+//let res = (await fetchJson('https://hercai.onrender.com/v3/hercai?question=' + q)).response
+let res = await fetchJson('https://hercai.onrender.com/turbo/hercai?question='+q)
+
+return await reply(res.reply)
+} catch (e) {
+reply(cantf)
+console.log(e)
+}
+})
+//==============================TURBO2============================
+
+cmd({
+    pattern: "turbo2",
+    alias: ["ai4","gpt4","turbo16k","chat4"],
+    react: '👾',
+    desc: desct,
+    category: "ai",
+    use: '.turbo16k <query>',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply(needus)
+//let res = (await fetchJson('https://hercai.onrender.com/v3/hercai?question=' + q)).response
+let res = await fetchJson('https://hercai.onrender.com/turbo-16k/hercai?question='+q)
+
+return await reply(res.reply)
+} catch (e) {
+reply(cantf)
+console.log(e)
+}
+})
+//==============================GEMINI============================
+
+cmd({
+    pattern: "gemini",
+    alias: ["ai5","gpt5","openai5","chat5"],
+    react: '👾',
+    desc: desct,
+    category: "ai",
+    use: '.gemini <query>',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply(needus)
+//let res = (await fetchJson('https://hercai.onrender.com/v3/hercai?question=' + q)).response
+let res = await fetchJson('https://hercai.onrender.com/gemini/hercai?question='+q)
+
+return await reply(res.reply)
+} catch (e) {
+reply(cantf)
+console.log(e)
+}
+})
+
